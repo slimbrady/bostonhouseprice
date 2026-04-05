3bca55c306a54b5488aed3805e41b744a8745f5f
slimbrady <bradypowelldpt@gmail.com>
2026-04-04 18:05:58 -0700

    Fix typo in model loading line

--- app_py.py
+++ app_py.py
@@ -12,7 +12,7 @@ import pandas as pd
 import joblib
 
 
-model- joblib.load(model.pkl)
+model.joblib.load(model.pkl)
 st.set_page_config(page_title = 'Boston house price predictor',
                    page_icon = '🏠',
                    layout = 'centered',
