a4ac316ab8895fd69c16c82cfd62c5ca68314c25
slimbrady <bradypowelldpt@gmail.com>
2026-04-04 18:36:21 -0700

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -38,7 +38,7 @@ Lstat = st.number_input("LSTAT - % lower status of the population", min_value=0.
 
 input_data = pd.DataFrame({
     "CRIM": [crim],
-    "ZN": [zn],
+    "Zn": [zn],
     "INDUS": [indus],
     "CHAS": [chas],
     "NOX": [nox],
